2cf9cb5996e1e6a855e22a1a7276c309c71c7c65
Greg Doud <gdoud@gmail.com>
2022-11-23 19:51:43 -0500

    commit for laptop reset

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Trace to file!");
+
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        var streamWriter = File.CreateText(Path.Combine(folderPath, "appTrace.log"));
+        var traceWriter = new System.Diagnostics.TextWriterTraceListener(streamWriter);
+        System.Diagnostics.Trace.Listeners.Add(traceWriter);
+
+        System.Diagnostics.Trace.AutoFlush = true;
+
+        System.Diagnostics.Trace.WriteLine("This is a trace message.");
+    }
+}
